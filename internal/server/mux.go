7ee1357c98edc40d2250e9a9c3b70d5fd686dc86
pan <saran.swpy@gmail.com>
2025-06-16 11:49:54 +0700

    feat: add infinite publishing func for refreshing message queue knowledge

--- internal/server/mux.go
+++ internal/server/mux.go
@@ -2,10 +2,12 @@ package server
 
 import (
 	"encoding/json"
+	"math/rand"
 	"my-oauth-server/internal/calculator"
 	"my-oauth-server/internal/oauth"
 	"my-oauth-server/internal/utils"
 	"net/http"
+	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -50,29 +52,37 @@ func test2(w http.ResponseWriter, req *http.Request) {
 }
 
 func testQueue(w http.ResponseWriter, req *http.Request) {
-	body := map[string]any{
-		"jobId": 123,
-		"name":  "Saran",
-		"stock": "NVDA",
-	}
-
-	jsonBody, err := json.Marshal(body)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		utils.WriteResponseBody(w, utils.ResponseBody{Error: err.Error()})
-		return
-	}
-
-	MainChannel.Publish(
-		"",
-		MainQueue.Name,
-		false,
-		false,
-		amqp.Publishing{
-			ContentType: "application/json",
-			Body:        jsonBody,
-		},
-	)
-
+	go infinitePublish(w)
 	utils.WriteResponseBody(w, utils.ResponseBody{Message: "done"})
+}
+
+func infinitePublish(w http.ResponseWriter) {
+	ticker := time.NewTicker(time.Millisecond * 100)
+	for {
+		body := map[string]any{
+			"id":    rand.Int31()%100 + 1,
+			"name":  "Saran",
+			"stock": "NVDA",
+			"price": rand.Int31(),
+		}
+
+		jsonBody, err := json.Marshal(body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			utils.WriteResponseBody(w, utils.ResponseBody{Error: err.Error()})
+			return
+		}
+
+		MainChannel.Publish(
+			"",
+			MainQueue.Name,
+			false,
+			false,
+			amqp.Publishing{
+				ContentType: "application/json",
+				Body:        jsonBody,
+			},
+		)
+		<-ticker.C
+	}
 }
